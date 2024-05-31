11581a22b7d336436d858800b40b93369ce41d85
Vincent Huisman <mail@vincent-huisman.nl>
2024-05-31 11:42:30 +0200

    Reduced the pixels required for the bitencode

--- BitEncode.ts
+++ BitEncode.ts
@@ -4,11 +4,13 @@ import {CharacterMap, GetMapCodeForChar} from './CharacterMap.js'
 import {Utils} from './Utils.js'
 
 export class BitEncode extends BaseEncoder {
+    private static readonly maxBits = 6
+
     encode(input: string): string[] {
         const output: string[] = []
         for (const char of input) {
             const code = GetMapCodeForChar(char)
-            for (let i = 0; i < 8; i++) {
+            for (let i = 0; i < BitEncode.maxBits; i++) {
                 if (Utils.IsBitSet(code, i)) {
                     output.push('white')
                 } else {
@@ -24,12 +26,12 @@ export class BitEncode extends BaseEncoder {
         let code = 0
         let output = ''
         for (let pixel of pixels) {
-            const bitIndex = (i % 8)
+            const bitIndex = (i % BitEncode.maxBits)
             if (pixel.r + pixel.g + pixel.b >= 255) {
                 const mask = 1 << bitIndex
                 code |= mask
             }
-            if (bitIndex === 8 - 1) {
+            if (bitIndex === BitEncode.maxBits - 1) {
                 output += CharacterMap[code]
                 code = 0
             }
